133677b465aaa643faf3c6b0ff7d06bad1277e6c
Shakarra McGuire <shakarramcguire@gmail.com>
2018-10-11 00:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 let newID = oldID + 1000000000;
+debugger
 if (currentAgeIsInteger) {
   ageIsValid = true;
 } else {
